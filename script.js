2ecc447b9dcd44573ed71e55ebb18a43cebf0616
Beh <b@kayuk.de>
2021-02-01 23:08:49 +0100

    Add changing of educational label to etepetete
    And cleanup JS

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+/**
+ * 
+ *  Global variables
+ * 
+ */
+
+let coloredLettersCounter = 0;
+const eskapadeLetterColorClasses = ["violet", "blue", "blue2", "green", "yellow", "orange", "pink", "red"];
+
+
+/**
+ * 
+ *  Small class for changing a letter's color on hover
+ *  and also changing the color of a part in the description.
+ * 
+ */
+
 class EskapadeLetter {
 
     constructor(letterElement, descriptionTextElement, colorClass) {
@@ -8,14 +25,18 @@ class EskapadeLetter {
     }
 
     initEventListeners() {
+        const reitsportDescription = document.getElementById("description-reitsport");
+        const educationalLabel = document.getElementById("educational-label");
+        
         this.letterElement.addEventListener("mouseover", () => {
             this.toggleColor();
 
-            const reitsportDescription = document.getElementById("description-reitsport");
             if(coloredLetterExists()) {
                 reitsportDescription.style.textDecoration = "line-through";
+                educationalLabel.innerText = "Etepetete";
             } else {
                 reitsportDescription.style.textDecoration = "";
+                educationalLabel.innerText = "Bildungssprachlich";
             }
         });
     }
@@ -35,6 +56,12 @@ class EskapadeLetter {
     }
 }
 
+/**
+ * 
+ *  Stuff for tracking overall eskapade letter states
+ * 
+ */
+
 function coloredLetterExists() {
     return coloredLettersCounter > 0;
 }
@@ -43,16 +70,17 @@ function allLettersAreColored() {
     return coloredLettersCounter === eskapadeLetterColorClasses.length;
 }
 
-let coloredLettersCounter = 0;
-
-const eskapadeLetterColorClasses = ["violet", "blue", "blue2", "green", "yellow", "orange", "pink", "red"];
+/**
+ * 
+ * Starting point 
+ * Get all the DOM elements for creating letter classes
+ * and create letters
+ * 
+ */
 
 window.onload = () => {
     const eskapadeLetterElements = document.getElementsByClassName("eskapade-letter");
     const coloredDescriptionParts = document.getElementsByClassName("colored-description");
-
-    console.log(eskapadeLetterElements);
-    console.log(coloredDescriptionParts);
     const eskapadeLetters = [];
 
     for (let i = 0; i < eskapadeLetterElements.length; i++) {
@@ -63,6 +91,4 @@ window.onload = () => {
         letter.initEventListeners();
         eskapadeLetters.push(letter);
     }
-
-    console.log(eskapadeLetters);
 }
